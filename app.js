4ddf7ad5cbb80868e96bb5537cad7e0125b3df3b
juanmi <juanmidelatorre@hotmail.es>
2018-01-21 21:22:36 +0100

    servicio get usuarios

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 
-
-
 // Inicializar variables
 var app = express();
+
+// Importar rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
 
 
 // Conexion a la base de datos
@@ -17,15 +20,12 @@ mongoose.connection.openUri('mongodb://localhost:27017/esteticaDB', (err, res) =
 })
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada corecctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
+
 
 
 // Escuchar peticiones
 app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', ' online');
-})
+});
